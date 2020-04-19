9728e3d405c97e3f3b00b5bb8f2e544509627fac
JP Verkamp <me@jverkamp.com>
2020-04-18 23:32:52 -0400

    Add plant count/height markers

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 onready var Tetromino = preload("res://scenes/Tetromino.tscn")
 onready var tetrominos = $Tetrominos
+onready var engine = $PixelEngine
+onready var score_count = $Scores/Count
+onready var score_height = $Scores/Height
 
 func spawn():
 	var child = Tetromino.instance()
@@ -14,7 +17,6 @@ func spawn():
 func _ready():
 	OS.window_size = Vector2(320, 640)
 	
-	var engine = $PixelEngine
 	for x in range(engine.WIDTH):
 		var wall_start = engine.HEIGHT - 1 - (randi() % 10)
 		
@@ -32,10 +34,23 @@ func _on_lock():
 
 func _physics_process(delta):
 	OS.set_window_title("TETRIS SAND | FPS: " + str(Engine.get_frames_per_second()))
-	
+					
 	if Input.is_action_just_pressed("ui_cancel"):
 		for tetromino in tetrominos.get_children():
 			tetromino.queue_free()
 			tetrominos.remove_child(tetromino)
 		spawn()
 		
+func _on_Scores_timeout():
+	var count = 0
+	var highest = engine.HEIGHT
+	
+	var found = false
+	for x in range(engine.WIDTH):
+		for y in range(engine.HEIGHT):
+			if engine.data[x][y] == engine.CELL.plant:
+				count += 1
+				highest = min(highest, y)
+	
+	score_count.text = "Plants: " + str(count)
+	score_height.text = "Tallest: " + str(engine.HEIGHT - highest)
